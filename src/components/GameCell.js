31bf0e9fc73a0f56b79ebadc5fc659edf550b0f0
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-16 08:17:06 -0900

    prevent click if cell has already been selected
    Co-authored-by: Christy Welch <christinamawel@gmail.com>

--- src/components/GameCell.js
+++ src/components/GameCell.js
@@ -9,6 +9,7 @@ class GameCell extends React.Component{
       currentImg: 0
     }
   }
+
   style = {
     border: ".2em solid black",
     width: "10em",
@@ -17,16 +18,19 @@ class GameCell extends React.Component{
   }
 
   clickFunc = () => {
-    this.props.theClickening(this.props.id);
-    switch(this.props.turn){
-      case 1:
-        this.setState({currentImg: 1})
-        break;
-      case 2:
-        this.setState({currentImg: 2})
-        break;
-      default:
-      return
+    const clickable = this.state.currentImg === 0;
+    if(clickable){
+      this.props.theClickening(this.props.id);
+      switch(this.props.turn){
+        case 1:
+          this.setState({currentImg: 1})
+          break;
+        case 2:
+          this.setState({currentImg: 2})
+          break;
+        default:
+        return
+      }
     }
   }
 
