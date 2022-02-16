1a9cd9b3144695b96c6a3b857ca07ea35175a781
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-15 15:12:34 -0900

    add GameCell Co-authored-by: Christy Welch <christinamawel@gmail.com>

---
+++ src/components/GameCell.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import BlackCat from '../img/cat-black.png';
+import WhiteCat from '../img/cat-white.png';
+
+class GameCell extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      currentImg: 0
+    }
+  }
+  style = {
+    border: ".2em solid black",
+    width: "10em",
+    height: "10em",
+    boxSizing: "border-box"
+  }
+
+  clickFunc = () => {
+    this.props.theClickening(this.props.id);
+    switch(this.props.turn){
+      case 1:
+        this.setState({currentImg: 1})
+        break;
+      case 2:
+        this.setState({currentImg: 2})
+        break;
+      default:
+      return
+    }
+  }
+
+  render() {
+    let currentImg = null;
+    switch(this.state.currentImg){
+      case 1:
+        currentImg = <img src={WhiteCat}/>;
+        break;
+      case 2:
+        currentImg = <img src={BlackCat}/>;
+        break;
+      default:
+        break;
+    }
+    return (
+      <div onClick={this.clickFunc} style={this.style}>
+        {currentImg}
+      </div>
+    );
+  }
+}
+
+export default GameCell;
